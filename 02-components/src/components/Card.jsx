b6393a30f6a575754a1b8a43690aa043bf4210c9
Arijit Garai <arijitgarai05@gmail.com>
2026-01-29 21:15:49 +0530

    02-components done

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div className="card">
+        <h1>Hi I am Arijit Garai</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, natus!</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
