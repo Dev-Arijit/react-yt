4aa69fb311fc3dfea1be51785eb824e263818112
Arijit Garai <arijitgarai05@gmail.com>
2026-02-10 11:05:36 +0530

    tailwind ready

---
+++ 06-tailwind/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className='h-screen w-full bg-gray-900'>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
